df754033d45b3aed0805cfeb4053ca81ef6dc0f3
Yeswanth Raghav T <tyraghav@outlook.com>
2019-08-29 22:50:05 +0530

    Added Banner

--- mywork/src/main/java/com/mywork/restServices/JobRestServices.java
+++ mywork/src/main/java/com/mywork/restServices/JobRestServices.java
@@ -1,9 +1,13 @@
 package com.mywork.restServices;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mywork.jsonPojo.Job;
 import com.mywork.services.JobServices;
 
 @RestController
@@ -12,4 +16,8 @@ public class JobRestServices {
 	@Autowired
 	JobServices jobServices;
 	
+	@GetMapping("/getAllJobs")
+	List<Job> getAllJobs(){
+		return jobServices.getAllJobs();
+	}
 }
